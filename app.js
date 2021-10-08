65b9187efde2d06a1154d283892a096bb131aeee
unknown <jidnyasasuki466@gmail.com>
2021-10-09 00:30:11 +0530

    operators in javascript

--- app.js
+++ app.js
@@ -1,5 +1,45 @@
-var arr= [1,2,3]
-console.log(arr)
-arr.push(5)
-console.log(arr)
-console.log(arr.length)
+//operators
+//arithmatic operators
+var a=90
+var b=10
+console.log('a + b =',a+b)
+console.log('a - b =',a-b)
+console.log('a * b =',a*b)
+console.log('a / b =',a/b)
+console.log('a ** b =',a**b)
+console.log('a % b =',a%b)
+
+//assignment operator
+b=11
+console.log('b',b)
+b++
+console.log('b',b)
+b--
+console.log('b',b)
+
+var c =10
+console.warn(c)
+console.warn(c++)
+console.warn(++c)
+
+//comparison opeators
+var c =10
+var d =5
+var e =10
+var f ='50'
+console.log(d>c)
+console.log(d<c)
+console.log(d==c)
+console.log(c==e)
+console.warn(c + e)
+console.warn(c + f)
+//logical operators
+console.log("true && true" , true && true)
+console.log("flase && true" , false && true)
+console.log("true && false" , true && false)
+console.log("false && false" , false && false)
+
+console.log("true || true" , true || true)
+console.log("flase || true" , false || true)
+console.log("true || false" , true || false)
+console.log("false || false" , false || false)
