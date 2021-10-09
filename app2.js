c45c634e93a0eb23975ac7656b7c6271f79f0f9f
unknown <jidnyasasuki466@gmail.com>
2021-10-09 18:49:04 +0530

    DOM API Selection M ethods

---
+++ app2.js
@@ -0,0 +1,16 @@
+var heading = document.getElementById('heading')
+var para = document.getElementsByClassName('para')
+var tags = document.getElementsByTagName('a')
+var query = document.querySelector('.para')
+
+//console.log(tags)
+console.log(query)
+/*
+console.log(heading)
+console.log(para)
+console.log(para.length)
+console.log(para[0])
+console.log(para[1])
+
+// para.push('jidnya')  no access to this method
+*/
