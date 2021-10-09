a2f15fab7130b2f72813ddae0e01f7dd8fddd2fa
unknown <jidnyasasuki466@gmail.com>
2021-10-09 22:06:43 +0530

    ARRAY METHODS

---
+++ app18.js
@@ -0,0 +1,12 @@
+//ARRAY METHODS
+
+//callback function
+var arr = ['go to gym','eat food','clean room']
+// var callbackfunction = (element, index) => {
+//     console.log(element,index)
+// }
+// arr.forEach(callbackfunction)
+
+arr.forEach( (element, index) => {
+    console.log(element,index)
+})
--- app2.js
+++ app2.js
@@ -1,3 +1,4 @@
+/*
 var heading = document.getElementById('heading')
 var para = document.getElementsByClassName('para')
 var tags = document.getElementsByTagName('a')
@@ -13,4 +14,33 @@ console.log(para[0])
 console.log(para[1])
 
 // para.push('jidnya')  no access to this method
+
+var heading = document.getElementById('heading')
+var body = document.querySelector('body')
+
+heading.innerHTML= "I Was Added By JS"
+heading.style ="color:red;font-size:2rem"
+
+heading.classList.add('five')
+heading.classList.remove('one')
+
+body.classList.remove('dark')
+
+
+//console.log(heading.classList)
+
 */
+//FOR LOOP
+var list = document.querySelector('ul')
+//console.log(list)
+
+var arr = ['GO TO GYM', 'COOK OATMEAL', 'EAT']
+arr.push('COMPLETE ASSIGNMENT')
+for (var i=0;i<arr.length ;i++)
+{
+    var element = document.createElement('li')
+    var textNode = document.createTextNode(arr[i])
+    element.appendChild(textNode)
+    list.appendChild(element)
+   
+}
